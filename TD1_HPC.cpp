2b5731d0b356719ab74045b4c645b6f73b783cc3
Hana LBATH <hana.lbath@insa-lyon.fr>
2019-02-28 09:53:39 +0100

    Q3

--- TD1_HPC.cpp
+++ TD1_HPC.cpp
@@ -6,28 +6,47 @@
 
 using namespace std;
 
-double frand(double fMin, double fMax){
+double Frand(double fMin, double fMax){
 		// génère un nombre aléatoire entre fmin et fmax
     double f = (double)rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
 }
 
-void randomfill(double* tab, int size){
+void Randomfill(double* tab, int size){
 	// remplit un tableau tab de taille size avec des doubles aléatoires (suivant une loi uniforme)
 
 	for(int i=0; i<size; ++i){
-		double number = frand(0., 2.); 
+		double number = Frand(0., 2.); 
 		tab[i]=number;
 	}
 }
 
-void affichetab(double* tab, int size){
+void Affichetab(double* tab, int size){
 	// affiche le contenu du tableau tab de taille size
-	cout << "affichage de tab : " <<endl;
+	cout << "Affichage du tableau : " <<endl;
 	for(int i=0; i<size; ++i){
 		cout << tab[i] << endl;
 	} 
 }
+
+double* Addtab(double* tab1, int size1, double* tab2, int size2){
+	// additionne 2 tableaux de taille respectives size1 et size2
+	double* tab3;
+	tab3= new double[size1*sizeof(double)]; 
+	if(size1==size2){
+		for(int i=0; i<size1; ++i){
+			tab3[i]=tab1[i]+tab2[i];
+		}
+		return tab3;
+	}
+	else{
+		cout << "ATTENTION : les tableaux n'ont pas la même taille" << endl;
+		return tab3;
+	}
+
+}
+
+
 
 int main(int argc, char** argv){
 
@@ -41,12 +60,26 @@ cout << "vect1=" << vect1[3] << endl;*/
 // avec un tableau
 int size=atoi(argv[1]); // donne la taille du tableau
 
-double* tab;
-tab = new double[size*sizeof(double)];
-randomfill(tab, size);
-affichetab(tab, size);
-delete tab;
+// Q1 et Q2
+double* tab1; // creation du tableau tab1
+tab1 = new double[size*sizeof(double)];
+Randomfill(tab1, size);
+Affichetab(tab1, size);
 
+
+// Q3
+double* tab2; // creation du tableau tab2
+tab2 = new double[size*sizeof(double)];
+Randomfill(tab2, size);
+Affichetab(tab2, size);
+
+double* tab3 = Addtab(tab1, size, tab2, size);
+Affichetab(tab3, size);
+
+// DELETES
+delete tab1;
+delete tab2;
+delete tab3;
 
 return 0;
 }
