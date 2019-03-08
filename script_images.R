f484df5f54e60d20a4554745ff2002593f1c1d79
Hana LBATH <hana.lbath@insa-lyon.fr>
2019-03-08 10:29:24 +0100

    creation des fichiers txt

--- script_images.R
+++ script_images.R
@@ -1,3 +1,3 @@
 
-faible_prod <- read.table("fichier.txt", header = TRUE, sep="\t", dec=",")
+faible_prod <- read.table("faible_prod.txt", header = TRUE, sep="\t", dec=",")
 plot( faible_prod$taille.vecteur, faible_prod$moy)
